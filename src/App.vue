66eb0f51ccceb715215485568d642b789b0d50e5
fuyachen <294143413@qq.com>
2023-03-04 10:41:09 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header></Header>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import url(./assets/css/global.css);
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="fly-footer">
+      <p>
+        <a href="/" target="_blank">tomic社区</a> 2023 &copy;
+        <a href="/" target="_blank">tomic.com 出品</a>
+      </p>
+      <p>
+        <a href="/template/fly/" target="_blank">github</a>
+        <a href="/jie/2461/" target="_blank">微信小程序</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
